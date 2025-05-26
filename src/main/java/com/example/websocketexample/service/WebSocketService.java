cd52f6db8578f097476b8086bbc9b3b677d18b94
Diego Brito <diego.brito@sptech.school>
2025-05-26 16:36:06 -0300

    initial commit

---
+++ src/main/java/com/example/websocketexample/service/WebSocketService.java
@@ -0,0 +1,18 @@
+package com.example.websocketexample.service;
+
+import com.example.websocketexample.service.dto.MessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebSocketService {
+
+  private final SimpMessagingTemplate template;
+  
+  public void sendMessage(MessageDto message) {
+    template.convertAndSend("/topic/message", message.getMessage());
+  }
+}
---
+++ src/main/java/com/example/websocketexample/service/dto/MessageDto.java
@@ -0,0 +1,13 @@
+package com.example.websocketexample.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class MessageDto {
+  
+  private String message;
+}
